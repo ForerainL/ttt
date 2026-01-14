de51b872cb16ad0f4bde9d9506fce437c652ec2d
ForerainL <kzmnlsy@gmail.com>
2026-01-15 00:01:16 +0800

    Adjust OrderBookRector snapshot semantics

---
+++ trade.py
@@ -0,0 +1,20 @@
+"""Trade schema helpers."""
+
+TRADE_COLUMNS = [
+    "skey",
+    "yyyymmdd",
+    "hhmmss_nano",
+    "date",
+    "time",
+    "clock_at_arrival",
+    "exch_clock_at_arrival",
+    "obe_seq_num",
+    "msg_seq_num",
+    "trade_id",
+    "trade_tpx",
+    "trade_tvl",
+    "bid_order_id",
+    "ask_order_id",
+    "msg_order_type",
+    "msg_order_flag",
+]
